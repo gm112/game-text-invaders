872b1b9cfe9c188934c608f18e6133f73dbde78d
Jonathan Basniak <jon@bluedreamers.com>
2025-02-23 15:17:41 -0500

    chore: add note

--- saturn/core/src/primitives/messages/job-message.primitive.ts
+++ saturn/core/src/primitives/messages/job-message.primitive.ts
@@ -1,3 +1,4 @@
+// FIXME: fix(saturn/core): rework job_message to use SABs instead. #Try to work in SharedArrayBuffers instead of ArrayBuffers as this could allow for more efficient sharing of data between workers.
 export interface type_job_message<type_payload = ArrayBuffer> {
   type: 'wakeup' | 'tick' | 'die' | 'confirmation_response' | 'failure_response'
   origin: string
