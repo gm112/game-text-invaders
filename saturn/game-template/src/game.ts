639d6fd5c68141759b08870ac993de163979fc0c
Jonathan Basniak <jon@bluedreamers.com>
2025-02-23 13:56:29 -0500

    chore: gobbly gook

--- saturn/game-template/src/game.ts
+++ saturn/game-template/src/game.ts
@@ -1,19 +1,12 @@
-import {
-  create_saturn_game,
-  type type_options_create_saturn_game,
-} from '@bluedreamers/saturn-core/kernel.js'
+import { create_saturn_game } from '@bluedreamers/saturn-core/kernel.js'
 
-const initial_game_state = {
-  player_hp: 100,
-  score: 0,
-}
-const game_options: type_options_create_saturn_game<typeof initial_game_state> =
-  {
-    max_workers: navigator.hardwareConcurrency ?? 1,
-    state: initial_game_state,
-  }
-
-const { $game_loop } = create_saturn_game(game_options)
+const { $game_loop } = create_saturn_game({
+  max_workers: navigator.hardwareConcurrency ?? 1,
+  state: {
+    player_hp: 100,
+    score: 0,
+  },
+})
 
 $game_loop.subscribe((state) => {
   console.log('[game] state:', state)
